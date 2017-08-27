1031745c080964f028e142ebc30ef35b33b751a5
Simon Hudon <simon.hudon@gmail.com>
2017-08-27 16:49:19 -0400

    added examples and a `rand` and `gen` monads

--- test/random.lean
+++ test/random.lean
@@ -1,7 +1,6 @@
 
 import system.io
 import data.bitvec
--- import control.applicative.
 
 open list io applicative
 
@@ -56,6 +55,152 @@ end generator
 
 universes u
 
-def random (α : Type u) := generator → generator × α
+namespace tactic.interactive
 
-def gen (α : Type u) := ℕ → random α
+open lean.parser
+open interactive
+open interactive.types
+
+local postfix `?`:9001 := optional
+local postfix *:9001 := many
+
+meta def funext1 (x : parse ident_ ?) : tactic unit := do
+`[apply funext],
+intro x
+
+meta def funext : parse ident_ * → tactic unit
+ | [] := return ()
+ | (x :: xs) := funext1 x >> funext xs
+
+end tactic.interactive
+
+def rand (α : Type u) := generator → generator × α
+
+namespace rand
+
+variables {α β γ : Type u}
+
+protected def pure (x : α) : rand α :=
+λ g, (g,x)
+
+protected def bind (x : rand α) (f : α → rand β) : rand β :=
+λ g,
+let r := x g in
+f r.2 r.1
+
+instance : has_bind rand :=
+⟨ @rand.bind ⟩
+
+instance : has_pure rand :=
+⟨ @rand.pure ⟩
+
+lemma bind_assoc (x : rand α) (f : α → rand β) (g : β → rand γ)
+: x >>= f >>= g = x >>= (λ i, f i >>= g) :=
+begin
+  funext sz,
+  simp [has_bind.bind,rand.bind],
+end
+
+lemma pure_bind (x : α) (f : α → rand β)
+: pure x >>= f = f x :=
+by refl
+
+lemma id_map (x : rand α)
+: x >>= pure ∘ id = x :=
+begin
+  funext i,
+  simp [function.comp,has_bind.bind,rand.bind],
+  cases x i, refl
+end
+
+end rand
+
+instance : monad rand :=
+{ pure := @rand.pure
+, bind := @rand.bind
+, bind_assoc := @rand.bind_assoc
+, pure_bind  := @rand.pure_bind
+, id_map := @rand.id_map }
+
+def gen (α : Type u) := ℕ → rand α
+
+namespace gen
+
+variables {α β γ : Type u}
+
+protected def pure (x : α) : gen α :=
+λ _, pure x
+
+protected def bind (x : gen α) (f : α → gen β) : gen β
+ | sz := do
+i ← x sz,
+f i sz
+
+instance : has_bind gen :=
+⟨ @gen.bind ⟩
+
+instance : has_pure gen :=
+⟨ @gen.pure ⟩
+
+lemma bind_assoc (x : gen α) (f : α → gen β) (g : β → gen γ)
+: x >>= f >>= g = x >>= (λ i, f i >>= g) :=
+begin
+  funext sz,
+  simp [has_bind.bind],
+  simp [gen.bind,monad.bind_assoc],
+end
+
+lemma pure_bind (x : α) (f : α → gen β)
+: pure x >>= f = f x :=
+begin
+  funext i,
+  simp [has_bind.bind],
+  simp [gen.bind,monad.pure_bind],
+  refl
+end
+
+lemma id_map (x : gen α)
+: x >>= pure ∘ id = x :=
+begin
+  funext i,
+  simp [has_bind.bind,function.comp,pure,has_pure.pure],
+  simp [gen.bind,gen.pure],
+  rw monad.bind_pure,
+  exact α,
+end
+
+end gen
+
+instance : monad gen :=
+{ pure := @gen.pure
+, bind := @gen.bind
+, bind_assoc := @gen.bind_assoc
+, pure_bind  := @gen.pure_bind
+, id_map := @gen.id_map }
+
+namespace tactic.interactive
+open tactic
+
+meta def slim_check : tactic unit := do
+t ← target,
+p ← is_prop t,
+when (¬ p) (fail "expecting a proposition"),
+`[apply of_as_true, trivial]
+
+meta def expect_failure (cmd : itactic) : tactic unit := do
+x ← try_core cmd,
+match x with
+ | (some x) := fail "tactic is expected to fail"
+ | none := admit
+end
+
+end tactic.interactive
+
+example : ∀ n : ℕ, n > n+1 :=
+by expect_failure { slim_check }
+
+example : ∀ n : ℕ, n < n+1 :=
+by expect_failure { slim_check }
+
+example : 1 < (2 : ℕ) :=
+by slim_check
