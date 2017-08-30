04303cc026ff3a3aef857fa6d0096f6a9741edbb
Simon Hudon <simon.hudon@gmail.com>
2017-08-30 14:53:02 -0400

    `failure` in `test_result` now carries a proof that the tested property is false. Additionally, variables are named in counter examples

--- test/slim_check/tactics.lean
+++ test/slim_check/tactics.lean
@@ -4,6 +4,24 @@ import ...test.slim_check.testable
 namespace tactic.interactive
 open tactic slim_check
 
+meta def expect_failure (cmd : itactic) : tactic unit :=
+λ s, match cmd s with
+| (interaction_monad.result.exception _ _ s') := (trace s' >> admit) s
+| (interaction_monad.result.success a s) :=
+   mk_exception "success_if_fail combinator failed, given tactic succeeded" none s
+end
+
+
+meta def trace_error (cmd : itactic) : tactic unit :=
+λ s,
+let r := cmd s in
+match r with
+| (interaction_monad.result.exception a b s') :=
+(trace "\nBEGIN error" >> trace s' >> trace "END error"
+  >> interaction_monad.result.exception a b) s'
+| (interaction_monad.result.success a s) := r
+end
+
 meta def revert_all : tactic unit := do
 xs ← local_context,
 match xs with
@@ -11,26 +29,40 @@ match xs with
  | [] := return ()
 end
 
-meta def slim_check : tactic unit := (do
+
+/-- build an instance of testable for the given proposition
+  -/
+meta def is_testable : tactic unit := do
+(do
+`(testable %%e) ← target,
+match e with
+ | (expr.pi n bi d t) := do
+    p ← is_prop d,
+    h ← to_expr ``(testable %%t) >>= λ e, tactic.assert `h (expr.pi n bi d e),
+    solve1 (tactic.intro1 >> is_testable),
+    if p
+    then tactic.applyc `slim_check.imp_dec_testable
+    else ((to_expr ``(slim_check.var_testable _ _ (some %%(reflect $ to_string n)))
+           >>= tactic.apply) ; apply_instance)
+ | _ := tactic.applyc `slim_check.de_testable
+end)
+<|> tactic.applyc `slim_check.de_testable
+
+meta def slim_check : tactic unit := do
+gs ← get_goals,
+(do
 revert_all,
 t ← target,
 p ← is_prop t,
 when (¬ p) (fail "expecting a proposition"),
-t' ← eval_expr Prop t,
-e ← to_expr ``(testable.run %%t),
-run ← eval_expr (gen test_result) e,
-let hinst : testable t' := ⟨ _, run ⟩,
-r ← run_io (@testable.check t' hinst),
-if r then admit else fail "found counter examples")
-<|> fail "found counter examples"
+cl ←  to_expr ``(testable %%t),
+hinst ← prod.snd <$> tactic.solve_aux cl is_testable,
+ e ← to_expr ``(psigma.mk %%t %%hinst : Σ' t', testable t'),
+ ⟨t',hinst⟩ ← eval_expr (psigma testable) e,
+ r ← run_io (@testable.check t' hinst),
+ if r then admit else fail "found counter examples")
+ <|> (set_goals gs >> fail "found counter examples")
 
 open interaction_monad.result.
-
-meta def expect_failure (cmd : itactic) : tactic unit :=
-λ s, match cmd s with
-| (interaction_monad.result.exception _ _ s') := (trace s' >> admit) s
-| (interaction_monad.result.success a s) :=
-   mk_exception "success_if_fail combinator failed, given tactic succeeded" none s
-end
 
 end tactic.interactive
--- test/slim_check/testable.lean
+++ test/slim_check/testable.lean
@@ -4,87 +4,106 @@ import ...test.slim_check.arbitrary
 universes u v
 
 variable α : Type u
-variable β : α → Sort v
-variable f : Type → Sort v
+variable β : α → Prop
+variable f : Type → Prop
 
 namespace slim_check
 
-inductive test_result
-| success
-| gave_up : ℕ → test_result
-| failure : (list string) → test_result
+inductive test_result (p : Prop)
+| success {} : test_result
+| gave_up {} : ℕ → test_result
+| failure : ¬ p → (list string) → test_result
 
-class testable (α : Sort u) :=
-  (run : gen test_result)
+class testable (p : Prop) :=
+  (run : gen (test_result p))
 
 open list
 
 
 open test_result
 
-instance imp_dec_testable (p : Prop) [decidable p] (β : p → Sort u)
+def add_to_counter_example (x : string) {p q : Prop}
+  (h : q → p)
+: test_result p → test_result q
+ | (failure Hce xs) := failure (mt h Hce) $ x :: xs
+ | success := success
+ | (gave_up n) := gave_up n
+
+def convert_counter_example {p q : Prop}
+  (h : q → p)
+: test_result p → test_result q
+ | (failure Hce xs) := failure (mt h Hce) xs
+ | success := success
+ | (gave_up n) := gave_up n
+
+instance imp_dec_testable (p : Prop) [decidable p] (β : p → Prop)
   [∀ h, testable (β h)]
 : testable (Π h, β h) :=
-⟨ _,
-  do
+⟨ do
     if h : p
-    then testable.run (β h)
+    then convert_counter_example ($ h) <$> testable.run (β h)
     else return $ gave_up 1 ⟩
-
-def add_to_counter_example (x : string) : test_result → test_result
- | (failure xs) := failure $ x :: xs
- | x := x
 
 instance all_types_testable [testable (f ℤ)]
 : testable (Π x, f x) :=
-⟨ (Π x, f x),
-  do
+⟨ do
     r ← testable.run (f ℤ),
-    return $ add_to_counter_example "ℤ" r ⟩
+    return $ add_to_counter_example "ℤ" ($ ℤ) r ⟩
 
-instance pi_testable [has_to_string α] [arbitrary α] [∀ x, testable (β x)]
+def var_testable [has_to_string α] [arbitrary α] [∀ x, testable (β x)]
+  (var : option string := none)
 : testable (Π x : α, β x) :=
-⟨ (Π x : α, β x),
-  gen.down $ do
+⟨ gen.down $ do
   x ← arby α,
   gen.up (do
     r ← testable.run (β x),
-    return $ add_to_counter_example (to_string x) r) ⟩
+    return $ match var with
+     | none := add_to_counter_example (to_string x) ($ x) r
+     | (some v) := add_to_counter_example (v ++ " := " ++ to_string x) ($ x) r
+    end) ⟩
+
+instance pi_testable [has_to_string α] [arbitrary α] [∀ x, testable (β x)]
+: testable (Π x : α, β x) :=
+var_testable α β
 
 instance de_testable {p : Prop} [decidable p] : testable p :=
-⟨ p, return $ if p then success else failure [] ⟩
+⟨ return $ if h : p then success else failure h [] ⟩
 
 section io
 
-variable (α' : Sort u)
+variable (α' : Prop)
 variable [testable α']
 
 variable [io.interface]
 
 open nat
 
-def retry (cmd : rand test_result) : ℕ → rand test_result
+variable {α'}
+
+def retry (cmd : rand (test_result α')) : ℕ → rand (test_result α')
  | 0 := return $ gave_up 1
  | (succ n) := do
 r ← cmd,
 match r with
  | success := return success
- | (failure xs) := return (failure xs)
+ | (failure Hce xs) := return (failure Hce xs)
  | (gave_up _) := retry n
 end
 
-def give_up_once (x : ℕ) : test_result → test_result
+def give_up_once (x : ℕ) : test_result α' → test_result α'
  | success := gave_up x
  | (gave_up n) := gave_up (n+x)
- | (failure xs) := failure xs
+ | (failure Hce xs) := failure Hce xs
 
-def testable.run_suite_aux : test_result → ℕ → rand test_result
+variable (α')
+
+def testable.run_suite_aux : test_result α' → ℕ → rand (test_result α')
  | r 0 := return r
  | r (succ n) := do
 x ← retry (testable.run α' (99 - n)) 10,
 match x with
  | success := testable.run_suite_aux r n
- | (failure xs) := return (failure xs)
+ | (failure Hce xs) := return (failure Hce xs)
  | (gave_up g) := testable.run_suite_aux (give_up_once g r) n
 end
 
@@ -96,7 +115,7 @@ x ← io.run_rand (testable.run_suite α'),
 match x with
  | success := return tt
  | (gave_up n) := io.put_str_ln ("Gave up " ++ repr n ++ " times") >> return tt
- | (failure xs) := do
+ | (failure _ xs) := do
    io.put_str_ln "\n===================",
    io.put_str_ln "Found problems!",
    io.put_str_ln "",
